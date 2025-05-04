e40a8151209aa7eb834572763cc007afcf13b38f
Gendarxml <nurinah904@gmail.com>
2025-05-04 11:38:00 +0800

    Update _worker.js

--- js/_worker.js
+++ js/_worker.js
@@ -75,8 +75,8 @@ export default {
     try {
       const url = new URL(request.url);
       const upgradeHeader = request.headers.get("Upgrade");
-      const CHECK_API_BASE = "https://api.bmkg.xyz"; // Get base URL from secrets
-      const CHECK_API = `${CHECK_API_BASE}/check?ip=`;
+      const CHECK_API_BASE = "api.ndeso.xyz"; // Get base URL from secrets
+      const CHECK_API = "https://api.ndeso.xyz/check?ip=${ip}:${port}";
       
       // Handle IP check
       if (url.pathname === "/check") {
